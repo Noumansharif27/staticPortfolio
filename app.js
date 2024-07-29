71ae509e9bce0ccca3149fbb8a34157e30a81594
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-29 10:35:44 +0500

    working on project-cards

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-let tl = gsap.timeline();
 let revealText = document.querySelectorAll(".reveal");
 let nav = document.querySelector("#nav");
+let cursor = document.querySelector("#cursor");
+let hoverElements = document.querySelectorAll(".hover-element");
+
+// hoverElements.forEach((el) => {
+//   el.addEventListener("mouseenter", () => {
+//     alert("hi");
+//   });
+// });
+
+// window.addEventListener("mousemove", (details) => {
+//   gsap.to("#cursor", {
+//     x: details.clientX,
+//     y: details.clientY,
+//     duration: 0.3,
+//     ease: Expo,
+//   });
+// });
 
 function smoothScroll() {
   gsap.registerPlugin(ScrollTrigger);
@@ -65,100 +81,167 @@ function reveal() {
 
 reveal();
 
-window.addEventListener("load", () => {
-  function loaderAnimation() {
-    tl.from("h1 .childSpan ", {
-      delay: 1,
-      x: "100%",
-      stagger: 0.5,
+function animatinOne() {
+  let tl = gsap.timeline();
+
+  tl.from("h1 .childSpan ", {
+    delay: 1,
+    x: "100%",
+    stagger: 0.5,
+    duration: 2,
+    ease: "Expo.easeInOut",
+  })
+    .to(".childSpan", {
+      y: "-100%",
       duration: 2,
       ease: "Expo.easeInOut",
     })
-      .to(".childSpan", {
-        y: "-100%",
-        duration: 2,
-        ease: "Expo.easeInOut",
-      })
-      .to(
-        "#loader",
-        {
-          height: 0,
-          duration: 1,
-          ease: "Circ.easeInOut",
-        },
-        "a"
-      )
-      .to(
-        "#green-div",
-        {
-          height: 0,
-          duration: 1.1,
-          ease: "Circ.easeInOut",
-        },
-        "a"
-      )
-      .to(nav, {
+    .to(
+      "#loader",
+      {
+        height: 0,
+        duration: 1,
+        ease: "Circ.easeInOut",
+      },
+      "a"
+    )
+    .to(
+      "#green-div",
+      {
+        height: 0,
+        duration: 1.1,
+        ease: "Circ.easeInOut",
+      },
+      "a"
+    )
+    .to(nav, {
+      opacity: 1,
+      delay: -1.5,
+    })
+    .to(
+      "#content-row-2 h2",
+      {
+        rotateX: 0,
         opacity: 1,
-        delay: -1.5,
-      });
-  }
+        duration: 1.5,
+      },
+      "content"
+    )
+    .to(
+      "#content-row-1 h2",
+      {
+        rotateX: 0,
+        opacity: 1,
+        duration: 1.5,
+      },
+      "content"
+    )
+    .to(
+      "#content-para-1",
+      {
+        top: "13%",
+        duration: 2,
+      },
+      "content"
+    )
+    .to(
+      "#content-para-2",
+      {
+        top: "13%",
+        duration: 2,
+      },
+      "content"
+    );
+}
 
-  loaderAnimation();
-});
+animatinOne();
 
-function homeAnimation() {
+function animationtwo() {
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: "#home",
       scrub: 2,
-      start: "top top",
-      end: "bottom 30%",
+      start: "-150% 100%",
+      end: "bottom 150%",
     },
   });
 
-  gsap.to(".para p", {
+  gsap.set(".para p", {
     y: "100%",
+  });
+
+  gsap.to(".para p", {
+    y: "0%",
+    duration: 0.5,
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: "#home",
+      start: "-20% 100%",
+      end: "bottom 150%",
+    },
   });
 
   tl.to(
     "#cards .card-1",
     {
-      rotate: "-3",
+      rotate: "-20",
       right: "45%",
+      duration: 100,
+      ease: "Circ.easeInOut",
+      delay: "-1",
     },
     "cardAnim"
   )
     .to(
       "#cards .card-2",
       {
-        rotate: "0",
-        right: "38%",
+        rotate: "-5",
+        right: "35%",
+        duration: 100,
+        ease: "Power3.easeInOut",
+        delay: "-1",
       },
       "cardAnim"
     )
     .to(
       "#cards .card-3",
       {
-        rotate: "10",
-        righ: "-10%",
+        rotate: "0",
+        right: "25%",
+        duration: 100,
+        ease: "Power3.easeInOut",
+        delay: "-1",
       },
       "cardAnim"
     );
 }
 
-gsap.to(
-  ".para p",
-  {
-    y: "0%",
-    stagger: 0.2,
-    scrollTrigger: {
-      trigger: "#home",
-      start: "top 50%",
-      end: "bottom 50%",
-      markers: true,
-    },
-  },
-  "carAnim"
-);
+// gsap;
 
-homeAnimation();
+animationtwo();
+
+let initilizePath = `M 100 40 Q 500 40 1100 40`;
+let finalPath = `M 100 40 Q 500 40 1100 40`;
+function pageOneSVG() {
+  let page1 = document.querySelector("#page-1");
+  page1.addEventListener("mousemove", (details) => {
+    initilizePath = `M 200 40 Q ${details.x} ${details.y} 1150 40`;
+
+    console.log(details.y);
+    gsap.to("svg path", {
+      attr: { d: initilizePath },
+      duration: 2,
+      ease: "Power3.out(1,0.2)",
+    });
+  });
+
+  page1.addEventListener("mouseleave", (details) => {
+    gsap.to("svg path", {
+      attr: { d: finalPath },
+      duration: 1.5,
+      ease: "elastic.out(1,0.2)",
+    });
+  });
+}
+
+pageOneSVG();
